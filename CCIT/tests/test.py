c98ab765f89858cd8ee722b2def986c1961fe289
rajatsen91 <rsen91@gmail.com>
2017-07-03 14:00:35 -0500

    more packaging

---
+++ CCIT/tests/test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from CCIT import *
+
+
+class TestCCIT(TestCase):
+    def test_datagen(self):
+        allsamples = generate_samples_cos()
+        m,n = allsamples.shape
+        self.assertTrue(m == 1000)
+        self.assertTrue(n == 22)
+
+    def test_CCIT(self):
+        allsamples = generate_samples_cos()
+        pval = CCIT(allsamples[:,0:1],allsamples[:,1:2],allsamples[:,2:22])
+        self.assertTrue(pval <= 1)
+
+        
